a39d159fe36366e9d157f2f3db3878c26f102392
erbrother <z15625129961@163.com>
2017-04-12 23:57:27 +0800

    2017/4/12 0000

--- src/js/components/mobie_index.js
+++ src/js/components/mobie_index.js
@@ -1,12 +1,41 @@
 import React from 'react';
 import MobieHeader from './mobie_header';
 import MobieFooter from './mobie_footer';
+import { Tabs } from 'antd';
 
+const TabPane = Tabs.TabPane;	
 export default class MobieIndex extends React.Component {
+	constructor() {
+		super()
+
+		this.state = {
+			current: 'top',
+			visible: false,
+			action: 'login',
+			hasLogined: false,
+			userNickName: '',
+			userid: 0
+		};
+
+		this.callback = this.callback.bind(this);
+	};
+
+	callback(e) {
+		console.log(e.key)
+	}
+
 	render () {
+
 		return (
 			<div>
 				<MobieHeader></MobieHeader>
+			  <Tabs defaultActiveKey="1" >
+			    <TabPane tab="头条" key="1"></TabPane>
+			    <TabPane tab="社会" key="2"></TabPane>
+			    <TabPane tab="国内" key="3"></TabPane>
+			    <TabPane tab="国际" key="4"></TabPane>
+			    <TabPane tab="娱乐" key="5"></TabPane>
+			  </Tabs>
 				<MobieFooter></MobieFooter>
 			</div>
 		)
--- src/js/components/pc_header.js
+++ src/js/components/pc_header.js
@@ -12,9 +12,7 @@ import {
 	CheckBox,
 	Modal,
  } from 'antd';
- import {Router, Route, hashHistory, Link} from 'react-router';
 
-// import {request} from 'request';
 
 const FormItem = Form.Item;
 const TabPane = Tabs.TabPane;
@@ -92,27 +90,21 @@ class PCHeader extends React.Component {
   		method: 'GET'
   	};
   	var formData = this.props.form.getFieldsValue();
-<<<<<<< HEAD
-  	fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=" + this.state.action + "&username="+formData.userName+"&password="+ formData.password +"&r_userName="+ formData.r_userName+"&r_password="+ formData.r_password +"&r_confirmPassword=" + formData.r_comfirmPassword, myFetchOptions)
-  	.then(response=>response.json())
-  	.then(json=>{
-  		if ( this.state.action === "register" && json === true) {
-=======
+
   	console.log(formData)
   	fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=register&username=userName&password=password&r_userName="+ formData.userName+"&r_password="+ formData.password +"&r_confirmPassword=" + formData.comfirmPassword, myFetchOptions)
   	.then(response=>response.json())
   	.then(json=>{
   		// console.log(json===true)
   		if (json === true) {
->>>>>>> 8e35382acc7023b18e157c890cc8c5141f178c7b
+
   			message.success('注册地成功');
   			// console.log(formData)
   			this.setState({
-<<<<<<< HEAD
+
   				userNickName: formData.r_userName,
-=======
   				userNickName: formData.userName,
->>>>>>> 8e35382acc7023b18e157c890cc8c5141f178c7b
+
   				hasLogined: true,
   				visible: false
   			})			
